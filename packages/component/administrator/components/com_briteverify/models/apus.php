5423c33127e5186d7b4a2fb82c9a9997fb1fd2ea
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-09-24 11:16:36 +0200

    Renamed default to apus. Small clean up.

---
+++ packages/component/administrator/components/com_briteverify/models/apus.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * ComBriteverify
+ *
+ * @author 		Joep van der Heijden <joep.van.der.heijden@moyoweb.nl>
+ */
+
+defined('KOOWA') or die('Restricted Access');
+
+class ComBriteverifyModelDefault extends KModelAbstract
+{
+    protected function _getParams()
+    {
+        return array();
+    }
+
+    protected function _getUrl()
+    {
+        return '';
+    }
+
+    protected function _getAccount()
+    {
+        $state = $this->_state;
+
+        // Get default API key if not defined in state
+        if (!$state->id) {
+            $account = $this->getService('com://admin/briteverify.model.accounts')->id($state->id)->getItem();
+        } else {
+            $account = $this->getService('com://admin/briteverify.model.accounts')->default(1)->getItem();
+        }
+
+        return $account;
+    }
+
+    protected function _doCurl($url, $params)
+    {
+        $url = $url . http_build_query($params);
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($curl);
+        $resultStatus = curl_getinfo($curl);
+
+        if($resultStatus['http_code'] == 200) {
+            $data = json_decode($response, true);
+            return $data;
+        } else {
+            return null;
+        }
+    }
+
+    public function getItem()
+    {
+        $url = $this->_getUrl();
+        $account = $this->_getAccount();
+        $params = $this->_buildQuery();
+        $params['apikey'] = $account->api_key;
+
+        $response = $this->_doCurl($url, $params);
+
+        $row = $this->getService('com://admin/briteverify.database.row.response');
+        $row->setData($response);
+
+        return $row;
+    }
+
+    public function getList()
+    {
+        throw new Exception('function getList is not supported.');
+    }
+}
---
+++ packages/component/administrator/components/com_briteverify/models/emails.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * ComBriteverify
+ *
+ * @author 		Joep van der Heijden <joep.van.der.heijden@moyoweb.nl>
+ */
+ 
+defined('KOOWA') or die('Restricted Access');
+
+class ComBriteverifyModelEmails extends ComBriteverifyModelApus
+{
+    public function __construct(KConfig $config = null)
+    {
+        parent::__construct($config);
+
+        $this->_state->insert('address', 'string', null, true);
+    }
+
+    protected function _getUrl()
+    {
+        return 'https://bpi.briteverify.com/emails.json?';
+    }
+
+    protected function _buildQuery()
+    {
+        $state = $this->_state;
+
+        $params = parent::_buildQuery();
+        $params['address'] = $state->address;
+
+        return $params;
+    }
+}
