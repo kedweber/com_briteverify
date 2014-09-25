4f55dec90632dc5b7812c30b2cff535140e0f91f
Jasper van Rijbroek <jasper@jvar.nl>
2014-09-25 10:15:47 +0200

    Proposed change in briteverify.

--- packages/component/administrator/components/com_briteverify/databases/rows/response.php
+++ packages/component/administrator/components/com_briteverify/databases/rows/response.php
@@ -5,7 +5,7 @@
  *
  * @author 		Joep van der Heijden <joep.van.der.heijden@moyoweb.nl>
  */
- 
+
 defined('KOOWA') or die('Restricted Access');
 
 class ComBriteverifyDatabaseRowResponse extends KDatabaseRowAbstract
@@ -13,20 +13,12 @@ class ComBriteverifyDatabaseRowResponse extends KDatabaseRowAbstract
 
     public function isVerified()
     {
-        if ($this->status == 'valid') {
-            return true;
-        }
+        $accepted = array(
+            'valid',
+            'unknown',
+            'accept_all'
+        );
 
-        if ($this->status == 'invalid') {
-            return false;
-        }
-
-        if ($this->status == 'unknown') {
-            return true;
-        }
-
-        if ($this->status == 'accept_all') {
-            return true;
-        }
+        return in_array($this->status, $accepted);
     }
 }
